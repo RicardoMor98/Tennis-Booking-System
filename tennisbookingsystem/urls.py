36ff46a41bf76580462e780c5445f3ecf5d5979e
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2024-12-05 15:03:29 +0000

    Amend urls on the projectfile

--- tennisbookingsystem/urls.py
+++ tennisbookingsystem/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main_app import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls'))
 ]
