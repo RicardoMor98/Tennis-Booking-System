d85f1bde1ab38959ae19f7beb1d9774342732a3e
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2024-12-05 15:04:12 +0000

    Amend templates directions on settings

--- tennisbookingsystem/settings.py
+++ tennisbookingsystem/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'tennisbookingsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'booking/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
